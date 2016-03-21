8c703f0eed76a7bc4ce0d5064fb056ccb9732b5f
xumenger <xumenger@126.com>
2016-03-21 23:44:17 +0800

    修改自测用例中错误的语法，目前自测OK

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -5,8 +5,8 @@ from app import create_app, db
 
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
-        selp.app = create_app('testing')
-        sel.app_context = self.app.app_context()
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.create_all()
 
